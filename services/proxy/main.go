a2697db21f385bc968c2b9352615d44021180ad8
Aspirel <jonmultimedia1@gmail.com>
2026-07-28 22:33:23 +0100

    Update proxy structure

--- services/proxy/main.go
+++ services/proxy/main.go
@@ -4,13 +4,14 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/mux"
+    "ghostplayer/proxy/services"
 )
 
 func main() {
-    InitMongo()
+    services.InitMongo()
 
     r := mux.NewRouter()
-    r.HandleFunc("/youtube/search", YoutubeSearchHandler).Methods("GET")
+    r.HandleFunc("/youtube/search", services.YoutubeSearchHandler).Methods("GET")
 
     log.Println("Proxy running on :8080")
     http.ListenAndServe(":8080", r)
